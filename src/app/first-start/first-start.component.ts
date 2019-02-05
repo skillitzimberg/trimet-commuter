3f9a4515bb1636d437f863bd9b1e2ef7596fcac7
Phil Mass and Scott Bergler <phil@>
2019-02-05 10:10:40 -0800

    add local storage functionality with userData

--- src/app/first-start/first-start.component.ts
+++ src/app/first-start/first-start.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from '../user-data.service'
 
 @Component({
   selector: 'app-first-start',
   templateUrl: './first-start.component.html',
-  styleUrls: ['./first-start.component.css']
+  styleUrls: ['./first-start.component.css'],
+  providers: [ UserDataService ]
 })
 export class FirstStartComponent implements OnInit {
 
-  constructor() { }
+  constructor( public userDataService: UserDataService ) {
+   }
 
   ngOnInit() {
+  }
+
+  saveMorningStop(stopId) {
+    this.userDataService.saveMorningStop(parseInt(stopId));
+  }
+
+  saveEveningStop(stopId) {
+    this.userDataService.saveEveningStop(parseInt(stopId));
   }
 
 }
--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -9,15 +9,16 @@ import { AuthService } from './auth.service';
 
 export class UserDataService {
   localStorageName: string = "trimetCommuterData";
-  userData: Object;
+  userData;
 
   constructor() {
     this.init();
    }
 
    init() {
-     this.initializeUserData();
+     this.userData = this.initializeUserData();
      this.userData = this.loadFromLocalStorage(this.userData);
+     //??? subscribe to observable for changes to database
    }
 
    initializeUserData() {
@@ -33,7 +34,7 @@ export class UserDataService {
 
    loadFromLocalStorage(userData) {
      if(window.localStorage) {
-       const data = window.localStorage.getItem(this.localStorageName);
+       const data = JSON.parse(window.localStorage.getItem(this.localStorageName));
        if(data) {
          return data;
        }
@@ -43,7 +44,36 @@ export class UserDataService {
 
    saveToLocalStorage(userData) {
      if(window.localStorage) {
-       window.localStorage.setItem(this.localStorageName, userData);
+       window.localStorage.setItem(this.localStorageName, JSON.stringify(userData));
      }
+   }
+
+   saveMorningStop(stopId) {
+     this.userData.stops.morning = [stopId];
+     this.updateUserData();
+   }
+
+   saveEveningStop(stopId) {
+     this.userData.stops.evening = [stopId];
+     this.updateUserData();
+   }
+
+   saveQuickStop(stopId) {
+     this.userData.stops.quick = [stopId];
+     this.updateUserData();
+   }
+
+   saveRecentStop(stopId) {
+     const recentSize = 5;
+     if(this.userData.stops.recent.unshift(stopId) > recentSize) {
+       this.userData.stops.recent = recentSize;
+     }
+     this.updateUserData();
+   }
+
+   updateUserData() {
+     this.saveToLocalStorage(this.userData);
+     console.log("update", this.userData);
+     //??? update observable to go into database
    }
 }
