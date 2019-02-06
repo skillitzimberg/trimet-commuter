2374f407c356cd5621ca5962d8b22c2d986f30cf
Ralph Perdomo <ralph@prdmo.com>
2019-02-06 09:58:52 -0800

    set logout event to null out local stop data

--- src/app/first-start/first-start.component.ts
+++ src/app/first-start/first-start.component.ts
@@ -4,7 +4,6 @@ import { AuthService } from '../auth.service';
 import { UserDataService } from '../user-data.service'
 import { StopService } from '../stop.service';
 
-
 @Component({
   selector: 'app-first-start',
   templateUrl: './first-start.component.html',
@@ -20,53 +19,29 @@ export class FirstStartComponent implements OnInit {
 
    morningId: number;
    eveningId: number;
-  //  quickId: number;
-  //  recentIds: number[];
-
 
   ngOnInit() {
-
     this.authService.user.subscribe( (user) => {
-      console.log(user)
-      if(user) {
-        this.userDataService.userData.subscribe((userData) => {
-          if(userData) {
+      if (user) {
+        this.userDataService.userData.subscribe( (userData) => {
+          if (userData) {
             this.morningId = userData['morning'];
             this.eveningId = userData['evening'];
-            // this.quickId = userData['quick'];
-            // this.recentIds = userData['recent'];
-          }
-
-          console.log(this.morningId);
-          console.log(this.eveningId);
-
+          } 
         });
-      } 
-      
-      // else {
-      //   this.morningId = undefined;
-      //   this.eveningId = undefined;
-      //   this.quickId = undefined;
-      //   this.recentIds = undefined;
-      // }
+      } else { 
+        this.morningId = null;
+        this.eveningId = null;
+      }
     });
-
-
-
   }
-  
 
   saveMorningStop(stopId) {
-    console.log(stopId)
     this.userDataService.saveMorningStop(parseInt(stopId));
-    console.log(`morning set: ${this.morningId}`)
   }
 
   saveEveningStop(stopId) {
-    console.log(stopId)
-
     this.userDataService.saveEveningStop(parseInt(stopId));
-    console.log(`evening set: ${this.eveningId}`)
-
   }
+
 }
