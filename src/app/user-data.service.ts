c2b49755b0060c005aad12b76798f31030c2babe
Phil Mass <philrmass@gmail.com>
2019-02-07 13:04:09 -0800

    fix login offset

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -19,7 +19,6 @@ export class UserDataService {
    init() {
      this.authService.user.subscribe((user) => {
        this.user = user;
-       console.log("user", user && user.uid);
        if(this.user) {
          this.userData = this.db.object(user.uid).valueChanges();
        } else {
